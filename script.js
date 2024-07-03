529d0955b4ef1fb3a217bda7120473f6ac9502f5
gabrielss97 <gabriel.ssalvador97@gmail.com>
2024-07-02 22:16:00 -0300

    envio projeto

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
 const daysEl = document.querySelector("#days");
 const hoursEl = document.querySelector("#hours");
-const minutesEl = document.querySelector("#minutes");
+const minsEl = document.querySelector("#mins");
 const secondsEl = document.querySelector("#seconds");
 
-const newYears = "1 Jan 2024";
+const dateLaunchGame = "23 Dec 2024";
 
-function countdown() {
-  const newYearsDate = new Date(newYears);
+// calcular data de hoje menos data de lançamento
+// calcular data completa dias, horas, minutos e segundos
+// atribuir resultado do calculo ao meu HTML
+// executar as minhas regras de negocio a cada segundo
+// toda vez que o numero só tiver um digito eu quero adicionar um 0 a esquerda dele
+
+function calculateSeconds() {
   const currentDate = new Date();
+  const dateLaunchGameDate = new Date(dateLaunchGame);
 
-  const totalSeconds = (newYearsDate - currentDate) / 1000;
+  return (dateLaunchGameDate - currentDate) / 1000;
+}
 
-  const days = Math.floor(totalSeconds / 3600 / 24);
-  const hours = Math.floor(totalSeconds / 3600) % 24;
-  const minutes = Math.floor(totalSeconds / 60) % 60;
-  const seconds = Math.floor(totalSeconds) % 60;
+function calculateDate() {
+  const days = Math.floor(calculateSeconds() / 3600 / 24);
+  const hours = Math.floor(calculateSeconds() / 3600) % 24;
+  const mins = Math.floor(calculateSeconds() / 60) % 60;
+  const seconds = Math.floor(calculateSeconds()) % 60;
 
-  daysEl.innerHTML = formatTime(days);
-  hoursEl.innerHTML = formatTime(hours);
-  minutesEl.innerHTML = formatTime(minutes);
-  secondsEl.innerHTML = formatTime(seconds);
+  return {
+    days,
+    hours,
+    mins,
+    seconds,
+  };
 }
 
 function formatTime(time) {
   return time < 10 ? `0${time}` : time;
+}
+
+function countdown() {
+  daysEl.innerHTML = calculateDate().days;
+  hoursEl.innerHTML = formatTime(calculateDate().hours);
+  minsEl.innerHTML = formatTime(calculateDate().mins);
+  secondsEl.innerHTML = formatTime(calculateDate().seconds);
 }
 
 countdown();
